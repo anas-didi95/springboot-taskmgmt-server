156c6f9b16e8b7f3743182b204972bfebd1dd198
Anas Juwaidi <anas.didi95@gmail.com>
2023-12-20 11:21:16 +0800

    ADD: Initial project

---
+++ ms-taskmgmt-cmd/src/main/java/com/anasdidi/mstaskmgmtcmd/MstaskmgmtcmdApplication.java
@@ -0,0 +1,13 @@
+package com.anasdidi.mstaskmgmtcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstaskmgmtcmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstaskmgmtcmdApplication.class, args);
+	}
+
+}
---
+++ ms-taskmgmt-cmd/src/test/java/com/anasdidi/mstaskmgmtcmd/MstaskmgmtcmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasdidi.mstaskmgmtcmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstaskmgmtcmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
